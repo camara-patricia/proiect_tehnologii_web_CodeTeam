08431e8f629bef6843b7dd89c91adf6390622ce7
Corina Chirita <chiritacorina23@stud.ase.ro>
2026-01-14 22:17:41 +0200

    Commit

--- server/index.js
+++ server/index.js
@@ -1,51 +1,44 @@
-"use strict"
+"use strict";
 
 const express = require("express");
 const sequelize = require("./sequelize");
 const cors = require("cors");
-// router
-// Import routers
+
 const eventRouter = require("./services/eventServices");
 const eventGroupRouter = require("./services/eventGroupServices");
 const userRouter = require("./services/userServices");
 const eventUserRouter = require("./services/eventUserServices");
 const authRouter = require("./services/authServices");
 
+const Event = require("./models/event");
+const EventGroup = require("./models/eventGroup");
+const User = require("./models/user");
+const EventUser = require("./models/eventUser");
 
-//Import models to create associations
-const Event =  require('./models/event');
-const EventGroup = require('./models/eventGroup');
-const User = require('./models/user');
-const EventUser = require('./models/eventUser');
+EventGroup.hasMany(Event, { foreignKey: "groupId", onDelete: "CASCADE" });
+Event.belongsTo(EventGroup, { foreignKey: "groupId" });
 
-EventGroup.hasMany(Event, { foreignKey: 'groupId', onDelete: 'CASCADE' });
-Event.belongsTo(EventGroup, { foreignKey: 'groupId' });
-
-Event.belongsToMany(User, { through: 'EventUser', foreignKey: 'eventId' });
-User.belongsToMany(Event, { through: 'EventUser', foreignKey: 'userId' });
+Event.belongsToMany(User, { through: "EventUser", foreignKey: "eventId" });
+User.belongsToMany(Event, { through: "EventUser", foreignKey: "userId" });
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
-
 app.use(
-  express.urlencoded({
-    extended: true,
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
   })
 );
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
-// Bind routers
 app.use("/api", eventRouter);
 app.use("/api", eventGroupRouter);
 app.use("/api", userRouter);
 app.use("/api", eventUserRouter);
 app.use("/api/auth", authRouter);
+
 app.get("/create", async (req, res, next) => {
   try {
     await sequelize.sync({ force: true });
@@ -55,23 +48,16 @@ app.get("/create", async (req, res, next) => {
   }
 });
 
-app.listen(8080, async () => {
-    console.log("Server started on http://localhost:8080")
+const PORT = process.env.PORT || 8080;
 
-    try {
-        await sequelize.authenticate();
-        console.log("Connection has been established successfully");
-
-    } catch (err) {
-        console.error("Unable to connect to the database:", err);
-    }
+app.listen(PORT, async () => {
+  try {
+    await sequelize.authenticate();
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-// Create a middleware to handle 500 status errors.
 app.use((err, req, res, next) => {
-  console.error("[ERROR]:" + err);
   res.status(500).json({ message: "500 - Server Error" });
 });
-
-
-
